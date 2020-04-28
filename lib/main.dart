f1d5abd49a3c581aa17bb7eca74e95dafa55e036
Neville Mathew <18580821+NevilleMthw@users.noreply.github.com>
2020-04-29 00:55:27 +0400

    Added app drawer and changes in pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
   home: Scaffold(
+    drawer: Drawer(
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+          title: Text(
+            'Glass',
+            style: TextStyle(
+          fontSize: 10.0,
+          letterSpacing: 1.0,
+          color: Colors.black,
+        ),
+            ),
+        ),
+        ListTile(
+          title: Text('Personal'),
+        ),
+        ListTile(
+          title: Text('Expenses'),
+        ),
+        ListTile(
+          title: Text('Random'),
+        ),
+        ],
+        
+      ),
+    ),
     backgroundColor: Colors.grey[600],
     appBar: AppBar(
       title: Text(
         'Glass',
         style: TextStyle(
           fontSize: 20.0,
-          fontWeight: FontWeight.bold,
           letterSpacing: 1.0,
           color: Colors.white,
+          fontFamily: 'Orbitron',
         ),
       ),
       centerTitle: true,
@@ -24,5 +50,4 @@ void main() => runApp(MaterialApp(
     floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
   ),
 ));
-
 
